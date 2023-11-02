f565d80fda2b2371780887d54135c1959da5044d
kassy11 <kotaro11kassy@gmail.com>
2023-11-02 23:54:43 +0900

    add docker-compose.yml

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+max_retry_count=3
+retry_interval=5
+
+retry_count=0
+while true; do
+  docker compose up -d && break
+
+  retry_count=$((retry_count + 1))
+  if [ $retry_count -eq $max_retry_count ]; then
+    echo "Error: command failed after $max_retry_count attempts"
+    break
+  fi
+
+  echo "Command failed. Retrying in $retry_interval seconds..."
+  sleep $retry_interval
+done
+
