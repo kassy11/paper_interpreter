8b5ab462a6a4713e93e1194820607a75f7ed24b9
Kotaro Kashihara <kotaro11kassy@gmail.com>
2023-11-03 23:29:35 +0900

    always specify pdf url and delete arxiv

--- paper.py
+++ paper.py
@@ -1,8 +1,6 @@
 import os
-import arxiv
 import urllib.request
 import os
-from urllib.parse import urlparse
 from logzero import logger
 from pdfminer.high_level import extract_text
 from utils import load_env
@@ -10,53 +8,37 @@ from utils import load_env
 load_env()
 
 
-def is_pdf(url):
-    _, ext = os.path.splitext(url)
-    if ext == ".pdf":
-        return True
-    else:
-        return False
+def _is_pdf(http_response_obj):
+    return http_response_obj.getheader("Content-Type") == "application/pdf"
 
 
-def get_arxiv_pdf_url(arxiv_url):
-    client = arxiv.Client()
-
-    id = _extract_id(arxiv_url)
-    search = arxiv.Search(id_list=[id])
-    results = list(client.results(search))
-    pdf_url = results[0].pdf_url
-    return pdf_url
-
-
-def _extract_id(url):
-    parsed_url = urlparse(url)
-    path = parsed_url.path
-    directories = path.split("/")
-    id = directories[-1]
-    return id
-
-
-def download_pdf(pdf_url, tmp_file_name):
-    is_success = True
-    logger.info(f"Downloding pdf from {pdf_url}...")
+def download_pdf(url, tmp_file_name):
+    logger.info(f"Downloading pdf from {url}...")
     try:
-        with urllib.request.urlopen(pdf_url) as web_file:
+        with urllib.request.urlopen(url) as web_file:
+            if not _is_pdf(web_file):
+                logger.warn(f"Content-type of {url} is not application/pdf.")
+                return False
             with open(tmp_file_name, "wb") as local_file:
                 local_file.write(web_file.read())
     except Exception as e:
-        logger.warning(f"Failed to download pdf from {pdf_url}.")
+        logger.warning(f"Failed to download pdf from {url}.")
         logger.warning(f"Exception: {str(e)}")
-        is_success = False
-    return is_success
+        return False
+    return True
 
 
 def read(tmp_file_name):
     logger.info(f"Reading pdf text from {tmp_file_name}...")
-    paper_text = extract_text(tmp_file_name)
+    paper_text = extract_text(tmp_file_name).strip()
 
     # 参考文献以降を削除
-    reference_pos = max(paper_text.find("References"), paper_text.find("REFERENCES"))
-    paper_text = paper_text[:reference_pos].strip()
+    reference_pos = max(
+        paper_text.find("References"),
+        paper_text.find("REFERENCES"),
+        paper_text.find("参考文献"),
+    )
+    paper_text = paper_text[:reference_pos]
     # 論文PDFを削除
     logger.info(f"Delete paper {tmp_file_name}.")
     os.remove(tmp_file_name)
